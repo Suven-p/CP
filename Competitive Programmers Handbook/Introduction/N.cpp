6844cf92352fd6394c72a928c1883c5a72b1c72d
Suven-p <pandeysuven@gmail.com>
2021-05-21 17:28:46 +0545

    Fourteenth

---
+++ Competitive Programmers Handbook/Introduction/N.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if defined(LOCALCODE) && !defined(ONLINE_JUDGE)
+#define TEST
+#endif
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define mp make_pair
+#define FOR(i, j, k, inc) for (int i = j; i < k; i += inc)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#ifdef TEST
+#define debug(x...) cerr << #x << x << endl;
+#else
+#define debug(x...)
+#endif
+
+void solve(int N, int from, int to, int aux)
+{
+    if (N == 1)
+    {
+        cout << from << " " << to << "\n";
+    }
+    else
+    {
+        solve(N - 1, from, aux, to);
+        cout << from << " " << to << "\n";
+        solve(N - 1, aux, to, from);
+    }
+}
+
+int main()
+{
+#ifdef TEST
+    freopen("/media/psuv/Data/CP/input.in", "r", stdin);
+    freopen("/media/psuv/Data/CP/output.out", "w", stdout);
+    freopen("/media/psuv/Data/CP/output.out", "w", stderr);
+    auto start = chrono::high_resolution_clock::now();
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << pow(2, N) - 1 << "\n";
+    solve(N, 1, 3, 2);
+
+#ifdef TEST
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    time_taken *= 1e-9;
+    cout << "Time taken by program is : " << endl
+         << fixed
+         << time_taken << setprecision(9) << " seconds" << endl;
+#endif
+    return 0;
+}
